2a9d3836d1e519057fe231b40be93cb3b5fb1350
Alikhan <jonasofan.gubaev@gmail.com>
2023-05-15 20:15:17 +0600

    search bar added

--- NobelLaureatesApp/Controller/ViewController.swift
+++ NobelLaureatesApp/Controller/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 class ViewController: UIViewController {
     
     private var laureatesData: [LaureateElement] = []
+    private var filteredLaureatesData: [LaureateElement] = []
+    private var isSearching: Bool = false
     
     private var tableView: UITableView = {
         var tableView = UITableView()
         tableView.separatorStyle = .none
         return tableView
+    } ()
+    
+    private let searchBar: UISearchBar = {
+        var searchBar = UISearchBar()
+        searchBar.placeholder = "Search Laureate by name"
+        searchBar.searchBarStyle = .minimal
+        return searchBar
     } ()
 
     override func viewDidLoad() {
@@ -22,16 +31,19 @@ class ViewController: UIViewController {
         self.navigationItem.title = "Laureates"
         self.view.backgroundColor = .white
         
-        [tableView].forEach { self.view.addSubview($0) }
+        [tableView, searchBar].forEach { self.view.addSubview($0) }
         
         setConstraints()
         initalizeTableView()
         getAllLaureates()
         customBackButton()
+        
+        searchBar.delegate = self
     }
     
     private func setConstraints() {
-        tableView.anchor(top: view.topAnchor, right: view.rightAnchor, bottom: view.bottomAnchor, left: view.leftAnchor, paddingRight: 16, paddingBottom: 50, paddingLeft: 16)
+        searchBar.anchor(top: view.topAnchor, right: view.rightAnchor, left: view.leftAnchor, paddingTop: 100, paddingRight: 16, paddingLeft: 16)
+        tableView.anchor(top: searchBar.bottomAnchor, right: view.rightAnchor, bottom: view.bottomAnchor, left: view.leftAnchor, paddingRight: 16, paddingBottom: 50, paddingLeft: 16)
     }
     
     private func initalizeTableView() {
@@ -57,13 +69,16 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return laureatesData.count
+        return isSearching ? filteredLaureatesData.count : laureatesData.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
                 as? LaureateCell else { return UITableViewCell() }
-        cell.laureate = laureatesData[indexPath.row]
+        let laureate: LaureateElement = isSearching
+                                            ? filteredLaureatesData[indexPath.row]
+                                            : laureatesData[indexPath.row]
+        cell.laureate = laureate
         return cell
     }
 }
@@ -76,8 +91,32 @@ extension ViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let laureateVC = LaureateViewController()
-        laureateVC.laureate = laureatesData[indexPath.row]
+        if isSearching {
+            laureateVC.laureate = filteredLaureatesData[indexPath.row]
+        } else {
+            laureateVC.laureate = laureatesData[indexPath.row]
+        }
         navigationController?.pushViewController(laureateVC, animated: true)
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+extension ViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filterLaureates(by: searchText)
+    }
+    
+    private func filterLaureates(by searchText: String) {
+        if searchText.isEmpty {
+            filteredLaureatesData = laureatesData
+            isSearching = false
+        } else {
+            filteredLaureatesData = laureatesData.filter({ laureate in
+                return laureate.fullName.en.lowercased().contains(searchText.lowercased())
+            })
+            isSearching = true
+        }
+        tableView.reloadData()
     }
 }
